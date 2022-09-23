07e88712ea648756c4c480e5b826b839ed2c6891
furuitakayuki <maki1904@icloud.com>
2022-09-23 20:28:15 +0900

    SnackBar

--- app/src/main/java/jp/techacademy/takayuk/floatingactionbutton/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/floatingactionbutton/MainActivity.kt
@@ -1,16 +1,21 @@
 package jp.techacademy.takayuk.floatingactionbutton
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Action"){
+                    Log.d("UI_PARTS", "Snackbarをタップした")
+                }.show()
         }
     }
 }
